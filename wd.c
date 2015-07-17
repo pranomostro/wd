f666bc28981e800a0658957d370400975032d6b1
adrian <a.regenfuss@gmx.de>
2015-07-18 01:10:13 +0200

    Shortened the code.

--- wd.c
+++ wd.c
@@ -5,14 +5,12 @@
 
 typedef unsigned long ulong;
 
-void ainput(char* s);
-void printdate(ulong year, ulong month, ulong day);
+void aninput(char* s);
 
-ulong isleap(ulong year);
+short isleap(ulong year);
+
 long dayofyear(ulong year, ulong month, ulong day);
 long daydiff(ulong orgyear, ulong orgday, ulong usryear, ulong usrday);
-
-char* daynameof(ulong year, ulong day);
 
 static unsigned daytab[2][13]=
 {
@@ -25,10 +23,12 @@ static char* dayname[7]=
 	"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
 };
 
-void ainput(char* s)
+void aninput(char* s)
 {
 	ulong year, month, day;
+	long diff;
 	errno=0;
+
 	sscanf(s, "%lu-%lu-%lu", &year, &month, &day);
 
 	if(errno||!year||!month||!day)
@@ -37,25 +37,18 @@ void ainput(char* s)
 		return;
 	}
 
-	printdate(year, month, day);
-}
+	diff=daydiff(1, 1, year, dayofyear(year, month, day));
 
-void printdate(ulong year, ulong month, ulong day)
-{
-	char* dname;
-
-	dname=daynameof(year, dayofyear(year, month, day));
-
-	if(strncmp(dname, "illegal date", strlen(dname))==0)
+	if(diff<0)
 	{
 		fprintf(stderr, "error: bad date format\n");
 		return;
 	}
 
-	printf("%lu-%lu-%lu:%s\n", year, month, day, dname);
+	printf("%lu-%lu-%lu:%s\n", year, month, day, dayname[diff%7]);
 }
 
-ulong isleap(ulong year)
+short isleap(ulong year)
 {
 	return ((year%4==0&&year%100!=0)||(year%400==0));
 }
@@ -88,15 +81,7 @@ long daydiff(ulong orgyear, ulong orgday, ulong usryear, ulong usrday)
 	return days;
 }
 
-char* daynameof(ulong year, ulong day)
-{
-	long diff=daydiff(1, 1, year, day);	 /*1-1-1 was a monday*/
 
-	if(diff<0)
-		return "illegal date";
-
-	return dayname[diff%7];
-}
 
 int main(int argc, char** argv)
 {
@@ -106,12 +91,12 @@ int main(int argc, char** argv)
 	if(argc>1)
 	{
 		for(count=1; count<argc; count++)
-			ainput(argv[count]);
+			aninput(argv[count]);
 		return 0;
 	}
 
 	while(fgets(input, 16, stdin)!=NULL)
-		ainput(input);
+		aninput(input);
 
 	return 0;
 }
