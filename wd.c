32734070e9ef6300c121cb1e5bf192c692e9f413
scharlatan <a.regenfuss@gmx.de>
2016-03-14 21:19:06 +0100

    Rewrote wd with bearable error handling.
    How was I able to release code this bad?

--- wd.c
+++ wd.c
@@ -5,7 +5,6 @@
 
 typedef unsigned long ulong;
 
-void aninput(char* s);
 short isleap(ulong year);
 long dayofyear(ulong year, ulong month, ulong day);
 long daydiff(ulong orgyear, ulong orgday, ulong usryear, ulong usrday);
@@ -21,77 +20,63 @@ static char* dayname[7]=
 	"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
 };
 
-void aninput(char* s)
-{
-	ulong year, month, day;
-	long diff;
-	errno=0;
-
-	sscanf(s, "%lu-%lu-%lu", &year, &month, &day);
-
-	if(errno||!year||!month||!day)
-	{
-		fprintf(stderr, "error: no date of format YEAR-MONTH-DAY found\n");
-		return;
-	}
-	/*1-1-1 was a monday*/
-	diff=daydiff(1, 1, year, dayofyear(year, month, day));
-
-	if(diff<0)
-	{
-		fprintf(stderr, "error: bad date format\n");
-		return;
-	}
-
-	printf("%lu-%lu-%lu:%s\n", year, month, day, dayname[diff%7]);
-}
-
 short isleap(ulong year)
 {
 	return ((year%4==0&&year%100!=0)||(year%400==0));
 }
 
-long dayofyear(ulong year, ulong month, ulong day)
-{
-	ulong i, leap;
-
-	leap=isleap(year);
-
-	if(day>daytab[leap][month]||month>12)
-		return -1;
-
-	for(i=1;i<month;i++)
-		day+=daytab[leap][i];
-	return day;
-}
-
 long daydiff(ulong orgyear, ulong orgday, ulong usryear, ulong usrday)
 {
 	ulong leaps, years, days;
-	if(!orgyear||!usryear||!orgday||!usrday)
-		return -1;
 
-	leaps=(usryear/4)-(usryear/100)+(usryear/400)+isleap(usryear);
+	leaps=((usryear-1)/4)-((usryear-1)/100)+((usryear-1)/400);
 	years=usryear-orgyear-leaps;
 	days=(years* 365)+(leaps*366)+usrday-orgday;
 
 	return days;
 }
 
+long dayofyear(ulong year, ulong month, ulong day)
+{
+	ulong i;
+
+	for(i=1;i<month;i++)
+		day+=daytab[isleap(year)][i];
+	return day;
+}
+
 int main(int argc, char** argv)
 {
-	long count;
-	char input[BUFSIZ];
+	int i;
+	char c;
+	ulong year, day, month;
 
 	if(argc>1)
-	{
-		for(count=1; count<argc; count++)
-			aninput(argv[count]);
-		return 0;
-	}
-
-	while(fgets(input, BUFSIZ, stdin)!=NULL)
-		aninput(input);
+		for(i=1; i<argc; i++)
+		{
+			errno=0;
+			sscanf(argv[i], "%lu-%lu-%lu", &year, &month, &day);
+			if(errno||!year||!month||month>12||!day||day>daytab[isleap(year)][month])
+			{
+				fprintf(stderr, "error: no date of format YEAR-MONTH-DAY found\n");
+				continue;
+			}
+			printf("%lu-%lu-%lu:%s\n", year, month, day,
+				dayname[daydiff(1, 1, year, dayofyear(year, month, day))%7]);
+		}
+	else
+		while(scanf("%lu-%lu-%lu", &year, &month, &day)!=EOF)
+		{
+			if(errno||!year||!month||month>12||!day||day>daytab[isleap(year)][month])
+			{
+				fprintf(stderr, "error: no date of format YEAR-MONTH-DAY found\n");
+				while((c=fgetc(stdin))!=EOF&&c!='\n');
+				continue;
+			}
+			printf("%lu-%lu-%lu:%s\n", year, month, day,
+				dayname[daydiff(1, 1, year, dayofyear(year, month, day))%7]);
+			errno=0;
+		}
 
 	return 0;
 }
